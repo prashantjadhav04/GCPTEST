834593dd328f6ae988681eb0efc5dd49306035a6
prashantjadhav04 <110183498+prashantjadhav04@users.noreply.github.com>
2025-04-01 14:59:03 +0530

    Create InputDemo.java

---
+++ InputDemo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class InputDemo{
+      public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      String name = sc.nextLine();
+      if(name = Prashant){
+       System.out.println("My name is Prashant");
+      }
+
+      }
+}
